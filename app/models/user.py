810edb4f812523b87a4f3b540c38b7f337af7757
Booma <boomashunmugam@gmail.com>
2026-07-08 19:08:42 +0530

    Configure Alembic and create initial users table

--- app/models/user.py
+++ app/models/user.py
@@ -10,8 +10,7 @@ class User(Base):
     __tablename__ = "users"
 
     id: Mapped[int] = mapped_column(
-        primary_key=True,
-        index=True
+        primary_key=True
     )
 
     name: Mapped[str] = mapped_column(
